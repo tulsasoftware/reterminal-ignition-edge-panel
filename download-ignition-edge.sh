247ce387265fbe206dbfe57b6d8e1648bc794965
Corey Thompson <corey.dwayne.thompson@gmail.com>
2023-10-21 09:19:12 -0500

    download / installation works for 32 and 64 bit

--- download-ignition-edge.sh
+++ download-ignition-edge.sh
@@ -1,27 +1,55 @@
 #!/bin/bash
 echo "downloading and installing ignition edge..."
+
 echo "creating folder structures..."
 sudo mkdir /opt/ignitionedge/
 cd /opt/ignitionedge/
+arch=$(getconf LONG_BIT)
 
-echo "downloading ignition edge... this is gonna take a while..."
-#download the ignition edge panel 8.12 from a public google drive folder
-fileid="1uvjl82zgeki73uSmqNHy8DThNIcT68uw"
-filename="ignition-edge-linux-aarch64.zip"
-curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
-
-echo "extracting the package..."
-sudo unzip ./ignition-edge-linux-aarch64.zip
-
-echo "setting file permissions..."
-sudo chmod +x ignition.sh ignition-gateway ignition-util.sh
-
-echo "note: it is suggested to configure ignition edge to launch on startup..."
-echo "(/opt/ignitionedge/ignition.sh start)"
-
-#remove the unextracted blob
-echo "cleaning up temporary files..."
-rm ./ignition-edge-linux-aarch64.zip
+echo "detected $arch bit operating system"
+case $arch in
+  32) 
+  echo "case 32"
+  fileid="1qjCmGOrj9Z5yw5i1Z_HtyZbPKAjF37XK"
+  filename="Ignition-Edge-linux-armhf-32-8.1.33.zip"
+  curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+  curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+  
+  echo "extracting the package..."
+  sudo unzip "./${filename}"
+  
+  echo "setting file permissions..."
+  sudo chmod +x ignition.sh ignition-gateway ignition-util.sh
+  
+  echo "note: it is suggested to configure ignition edge to launch on startup..."
+  echo "(/opt/ignitionedge/ignition.sh start)"
+  
+  #remove the unextracted blob
+  echo "cleaning up temporary files..."
+  rm "./${filename}"
+  ;;
+  
+  #assume 64 bit if not 32 bit
+  *) 
+  echo "case 64" 
+  fileid="1uvjl82zgeki73uSmqNHy8DThNIcT68uw"
+  filename="ignition-edge-linux-aarch64.zip"
+  curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+  curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+  
+  echo "extracting the package..."
+  sudo unzip "./${filename}"
+  
+  echo "setting file permissions..."
+  sudo chmod +x ignition.sh ignition-gateway ignition-util.sh
+  
+  echo "note: it is suggested to configure ignition edge to launch on startup..."
+  echo "(/opt/ignitionedge/ignition.sh start)"
+  
+  #remove the unextracted blob
+  echo "cleaning up temporary files..."
+  rm "./${filename}"
+  ;;
+esac
 
 echo "installation complete."
